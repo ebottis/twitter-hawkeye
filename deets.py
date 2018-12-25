cea2f53fc6a359085db07e8907a8720513895d52
ebottis <ebottis4@gmail.com>
2018-12-25 17:26:52 -0500

    added deets config file

---
+++ deets.py
@@ -0,0 +1,16 @@
+##card details
+card = ""
+env = "dev"
+
+##Twilio
+twilio_sid = ''
+twilio_token = ''
+twilio_phone =''
+my_phone = ''
+
+
+##Twitter
+twitter_consumer_key = ''
+twitter_consumer_secret = ''
+twitter_access_token_key = ''
+twitter_access_token_secret = ''
